f872bfcf168118208d6f2715ba6eca99e13d4ecd
Jagruti kansurkar <Jagrutivk25@gmail.com>
2024-07-17 15:34:57 +0530

    created form

--- src/components/contactList/contactList.js
+++ src/components/contactList/contactList.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./contactList.css";
+import CreateContactForm from "../form/createContactForm";
 
 function ContactList() {
-  return <div className="contactList-container"></div>;
+  return (
+    <div className="contactList-container">
+      <CreateContactForm />
+    </div>
+  );
 }
 
 export default ContactList;
--- src/components/form/createContactForm.js
+++ src/components/form/createContactForm.js
@@ -2,7 +2,70 @@ import React from "react";
 import "./createContactForm.css";
 
 function CreateContactForm() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <ul>
+          <li>
+            <label>First Name : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>Last Name : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>DOB : </label>
+            <input type="number"></input>
+          </li>
+          <li>
+            <label>Gender : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>Email : </label>
+            <input type="email"></input>
+          </li>
+          <li>
+            <label>Phone No. : </label>
+            <input type="number"></input>
+          </li>
+          <li>
+            <label>Street Address : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>Street Address : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>City : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>State : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>Country : </label>
+            <input type="text"></input>
+          </li>
+          <li>
+            <label>Zip Code : </label>
+            <input type="number"></input>
+          </li>
+          <li>
+            <label>Social Media Link : </label>
+            <input type="url"></input>
+          </li>
+          <li>
+            <label>Work: </label>
+            <input type="text"></input>
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
 }
 
 export default CreateContactForm;
